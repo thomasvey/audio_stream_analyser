db506ae87c5f27b1e6ecdaacf45ebdb9a1553269
Tinka <mail@thomasvey.de>
2022-12-08 12:20:27 +0100

    add waveform ringbuffer

--- lib_plot.py
+++ lib_plot.py
@@ -2,15 +2,27 @@ import time
 from matplotlib import pyplot as plt
 from scipy.fftpack import fft
 import numpy as np
+import matplotlib.cm as cm
 
 class fast_plot():
 
-    def __init__(s, frames = 3200, spr=96000) -> None:
+    sub_sampling = 10
+    frame_ss = 3200/sub_sampling
+    
+    def __init__(s, spr=96000, frame = 3200, disp_win=1, audio_ch=1) -> None:
 
-        t_frame = 1/(spr/frames)
+        # some math and create buff
+        s.frame_ss = int(frame / s.sub_sampling)
+        t_frame = 1/(spr/frame) 
+        s.num_fames = (spr * disp_win) / frame
+
+        # Waveform
+        s.wave_buffer = np.zeros([int(s.frame_ss*s.num_fames),audio_ch], dtype=int)
+        print(np.shape(s.wave_buffer))
+        
         s.xt = np.linspace( start = 0, 
-                            stop  = t_frame,
-                            num   = frames)
+                            stop  = int(t_frame*s.num_fames),
+                            num   = int(s.frame_ss*s.num_fames))
 
         s.fig, s.ax = plt.subplots(nrows=2, ncols=1)
         s.fig.set_size_inches(15,15)
@@ -21,14 +33,14 @@ class fast_plot():
         s.ax[0].set_xlabel('Time (s)')
         s.ax[0].set_ylabel('Amplitude')
         s.ax[0].set_ylim([-1.1, 1.1])
-        s.ax[0].set_xlim(0, t_frame)
+        s.ax[0].set_xlim(0, int(t_frame*s.num_fames))
 
 
         #s.ax1.set_xlim(s.x.min(), s.x.max())
         # s.ax[1].set_xscale('log')
         # s.ax[1].set_ylim([-1.1, 1.1])
         # s.ax[1].set_xlim(10**(-6), 10**(-5))
-
+        
         s.nyquist_shannon = int(spr / 2)       
         s.xt_fft = np.arange(s.nyquist_shannon)
         s.plt_fft, = s.ax[1].plot([])      
@@ -46,34 +58,50 @@ class fast_plot():
 
         plt.show(block=False)
         s.t_start = time.time()
+        
+    def update_plot(s, i, data, spr=96000, frame=3200):
 
-    def update_plot(s, i, data, spr=96000):
-
+        t0 = time.time()
         # show fps
         tx = 'Mean Frame Rate:\n {fps:.3f}FPS'.format(fps= ((i+1) / (time.time() - s.t_start)) ) 
         s.text.set_text(tx)
+        
+        # fill ring buffer
+        s.wave_buffer = s.wave_buffer[s.frame_ss:]
+        s.wave_buffer = np.append(s.wave_buffer, data[::s.sub_sampling])
 
         # display waveform
-        s.waveform.set_data(s.xt, data)
+        s.waveform.set_data(s.xt, s.wave_buffer)
 
+        # plot ax0 -- wafeform
+        s.fig.canvas.restore_region(s.ax0background)
+        s.ax[0].draw_artist(s.text)
+        s.ax[0].draw_artist(s.waveform)
+        s.fig.canvas.blit(s.ax[0].bbox)
+        
+        t1 = time.time()
+        
         # display fft
         data = data.reshape(-1)
         sig_fft = np.abs(fft(data, spr))
         sig_fft = sig_fft[:s.nyquist_shannon]
         s.plt_fft.set_data(s.xt_fft, sig_fft)
 
-        # plot ax0
-        s.fig.canvas.restore_region(s.ax0background)
-        s.ax[0].draw_artist(s.text)
-        s.ax[0].draw_artist(s.waveform)
-        s.fig.canvas.blit(s.ax[0].bbox)
-
-        # plot ax1
+        # plot ax1 - fft
         s.fig.canvas.restore_region(s.ax1background)
         s.ax[1].draw_artist(s.plt_fft)
         s.fig.canvas.blit(s.ax[1].bbox)
 
+        t2 = time.time()
+        
         s.fig.canvas.flush_events()
+        
+        t3 = time.time()
+        
+        e1 = t1 - t0
+        e2 = t2 - t1
+        e3 = t3 - t2
+        print(f'wave {e1:.4f} \t fft {e2:.4f} \t show {e2:.4f}',end='')
 
 
 if __name__ == '__main__':
--- lib_soundcard.py
+++ lib_soundcard.py
@@ -17,39 +17,58 @@ def print_speaker():
 
 def print_mic():
     #get a list of all microphones:
-    mics = sc.all_microphones()
+    mics = sc.all_microphones(include_loopback=True)
     for m in mics:
         print(m)
     print('-'*30)
 
 def loop_back():
-    s_tinka = sc.get_speaker('Tinka')
-    print(s_tinka)
 
-    #m_tinka = sc.get_microphone("Tinka")
+    mic_name = "Speaker"
+    mic_name = "Monitor of LE-Tinka"
+    #m_tinka = sc.get_microphone(mic_name, include_loopback=True)
+    
     m_tinka = sc.default_microphone()
     print(m_tinka)
 
+    #s_tinka = sc.get_speaker('Tinka')
+    #print(s_tinka)
+
     # 96000spr/s / 30f/s = 3200frames 
-    spr = 96000
-    frames = 3200
+    fps = 30        # plot frame rate
+    spr = 48000     # Messpunktabstand
+    frame = int(spr/fps) # Messpunktfreihe
+    disp_win = 5    # anzuzeigendes Zeitfenster in s
+    
+    fp = plt.fast_plot(spr, frame, disp_win)
 
-    fp = plt.fast_plot(frames, spr)
-
-    with m_tinka.recorder(samplerate=spr, channels=[-1]) as mic, \
-        s_tinka.player(samplerate=spr) as sp:
+    with m_tinka.recorder(samplerate=spr, channels=[-1]) as mic: #, \
+        #s_tinka.player(samplerate=spr) as sp:
+    
+        print(1/fps)
         for i in range(10000):
-            
+            t0 = time.time()    
             # data.shape = (frames, channel)
-            data = mic.record(numframes=int(frames))
-
+            data = mic.record(numframes=int(frame))
+            
+            t1 = time.time()
+            #time.sleep(0.03)
             #sp.play(data)
 
-            fp.update_plot(i, data)
-
+            fp.update_plot(i, data, spr, frame)
+            t2 = time.time()            
             #print(f'{time.time():3.1f} {len(data)} {len(fft_out)}')
+            
+            e0 = t2 - t0
+            e1 = t1 - t0
+            e2 = t2 - t1
+            if(e1 < 0.0001):
+                print("Warning: Programm to slow")
+            
+            print(f'\t| total {e0:.4f}\t rec{e1:.4f} \t plot {e2:.4f}')
 
 if __name__ == '__main__':
+    #print_mic()
     loop_back()
     #fft_sin()
 
